d9c50a2349df6334da28436a3ba374a638efe96f
himanshuchauhn <hc9346356@gmail.com>
2026-09-16 21:54:10 +0530

    Fix import paths to use uppercase 'C' in Components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Navbar from "../components/Navbar";
-import CategoryMenu from "../components/CategoryMenu";
-import FoodItems from "../components/FoodItems";
-import Cart from "../components/Cart";
+import Navbar from "../Components/Navbar";
+import CategoryMenu from "../Components/CategoryMenu";
+import FoodItems from "../Components/FoodItems";
+import Cart from "../Components/Cart";
 const Home = () => {
   return (
     <>
